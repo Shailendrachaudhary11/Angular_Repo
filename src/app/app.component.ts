b170b459273a50fda23c4a86a4fbdae70ef4202d
Shailendrachaudhary11 <shailendrachaurdhary0008@gmail.com>
2025-03-22 20:59:59 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,18 @@ export class AppComponent {
   message="hello from BridgeLabz using one way Data Bindings";
   logoUrl="assets/logo-bridge.avif";
   userName: string= ' ';
+  errorMessage: string= '';
+
+  validateName(){
+    const regexPattern=/^[A-Z][a-zA-Z]{2,}$/;
+    if(!regexPattern.test(this.userName)){
+      this.errorMessage="Invalid Name! Name must be started with captial letter and must have minimum 2 letters";
+      
+    }
+    else{
+      this.errorMessage=" ";
+    }
+  }
 
   openBridgeLabzSite(){
     window.open('https://www.bridgelabz.com', '_blank');
